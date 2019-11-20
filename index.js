2b7ec7223d62672731adfc497cf30a294797d71c
Fabricio Catae <fcatae@hotmail.com>
2019-11-20 18:02:02 -0300

    add soap and express

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hello world')
+var express = require('express')
+var app = express();
+var soap = require('soap');
+
+var url = 'http://localhost:8081/service/BasicHttpBinding_IOrderService?wsdl';
+
+app.get('/start', (req, res) => {
+    var args = {};
+    soap.createClient(url, function(err, client) {
+        client.GetOrders(args, function(err, result) {
+            var orders = 
+                        result.GetOrdersResult.Order.map( order => {
+                            return order.Id;
+                        })
+
+            res.send({
+                mensagem: "boa noite!",
+                usuario: "Fabricio",
+                pedidos: orders
+            })
+        });
+    });
+
+});
+
+app.listen(8080)
