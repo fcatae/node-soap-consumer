e5b502cdb5808a3ef46ef49a9860a6eeb4495df9
Fabricio Catae <fcatae@hotmail.com>
2019-11-20 22:22:44 -0300

    use localhost

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ var express = require('express')
 var app = express();
 var soap = require('soap');
 
-var url = 'http://localhost:8081/service/BasicHttpBinding_IOrderService?wsdl';
+var url = 'http://localhost:5000/pedidos?wsdl';
 
 app.get('/start', (req, res) => {
     var args = {};
     soap.createClient(url, function(err, client) {
-        client.GetOrders(args, function(err, result) {
-            var orders = 
-                        result.GetOrdersResult.Order.map( order => {
-                            return order.Id;
-                        })
+        client.ListarPedidos(args, function(err, result) {
+
+            var orders = [];
+
+            result.ListarPedidosResult.Pedido.forEach( pedido => {
+                var p = {
+                    id: pedido.Id,
+                    nome: pedido.Username
+                }
+
+                if( p.nome == 'Fabricio' ) {
+                    orders.push(p.id);
+                }
+            })
+
+            console.log(orders)
 
             res.send({
                 mensagem: "boa noite!",
@@ -23,4 +34,5 @@ app.get('/start', (req, res) => {
 
 });
 
+console.log("server is listening on 8080");
 app.listen(8080)
